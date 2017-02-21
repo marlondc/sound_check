3a0a4d63b29dafdb0799caafc20126e0b4f8a033
Marlon DC <marlondc@thisismn.com>
2017-02-21 22:22:20 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(process.env.port || 3000, function() {
+  console.log('app listening on port: ' + this.address().port);
+});
